e69fc5c5facd9c3916826de96c48ba3b856d5436
savaliyanisarg <nsavaliya390@rku.ac.in>
2025-01-01 17:59:01 +0530

    xml file

---
+++ WebForm1.aspx
@@ -0,0 +1,25 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="_6ASP.net.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:AdRotator ID="AdRotator1" runat="server" AdvertisementFile="~/XMLFile1.xml" />
+        </div>
+        <br />
+        <asp:FileUpload ID="FileUpload1" runat="server" />
+        <br />
+        <br />
+        <asp:Button ID="Button1" runat="server" OnClick="Button1_Click" Text="Button" />
+        <br />
+        <br />
+        <asp:Label ID="Label1" runat="server" Text="Label"></asp:Label>
+        <br />
+    </form>
+</body>
+</html>
---
+++ WebForm1.aspx.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace _6ASP.net
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            if (FileUpload1.HasFile) 
+            {
+                try 
+                { 
+                    string filename = FileUpload1.FileName;
+                    string path = Server.MapPath("~/FILES")+filename;
+                    FileUpload1.SaveAs(path);
+                    Label1.Text = "file upload succfully";
+                }
+                catch (Exception ex)
+                {
+                    Label1.Text += ex.Message;
+                }
+            }
+        }
+    }
+}
